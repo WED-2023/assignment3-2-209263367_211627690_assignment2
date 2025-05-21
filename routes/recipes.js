41d93f058d18661026c035915b14ff0847d3977c
Asaf Zenou <zenoua@post.bgu.ac.il>
2025-05-21 19:33:59 +0300

    Now its working for people who love spoon

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,14 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+router.get("/random", async (req, res, next) => {
+  try {
+    const recipes = await recipes_utils.getRandomRecipes();
+    res.send(recipes);
+  } catch (error) {
+    next(error);
+  }
+});
 
 /**
  * This path returns a full details of a recipe by its id
@@ -16,6 +24,7 @@ router.get("/:recipeId", async (req, res, next) => {
     next(error);
   }
 });
+
 
 
 
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
-
+require("dotenv").config();
 
 
 /**
@@ -37,4 +37,30 @@ async function getRecipeDetails(recipe_id) {
 
 
 
-exports.getRecipeDetails = getRecipeDetails;
+
+async function getRandomRecipes() {
+  try {
+    const response = await axios.get(`${api_domain}/random`, {
+      params: {
+        number: 3,
+        apiKey: process.env.spooncular_apiKey
+      }
+    });
+
+    const recipeIDs = response.data.recipes.map(r => r.id);
+    const promises = recipeIDs.map(getRecipeDetails);
+    const details = await Promise.all(promises);
+    return details;
+
+  } catch (error) {
+    console.error("Error in get3RandomRecipes:", error.response?.data || error.message);
+    throw error;
+  }
+}
+
+
+module.exports = {
+  getRecipeInformation,
+  getRecipeDetails,
+  getRandomRecipes
+};
